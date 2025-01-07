82c3d6848e2a78e6e130725830618be22d00f2c6
Bronwen1967 <bron67@hotmail.com>
2025-01-07 08:44:46 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity if needed. Currently, this script is a placeholder.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Navigation bar script loaded!");
+});
